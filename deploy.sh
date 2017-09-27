d0ae5d0814e33c5a195745231302b36c96b72d4c
Keichi Takahashi <keichi.t@me.com>
2017-09-27 20:12:49 +0900

    Add cache control headers to files uploaded to S3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 stack build
-aws s3 sync _site/ s3://keichi.net --delete --exclude ".DS_Store"
 stack exec site build
+aws s3 sync _site/ s3://keichi.net --delete --exclude ".DS_Store" --cache-control "max-age=86400"
